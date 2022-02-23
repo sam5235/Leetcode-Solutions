2fda2df544485ebed1d1c9cf394845231125a25a
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-02-23 17:19:58 +0300

    Time: 518 ms (82.32%), Space: 124.1 MB (16.40%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,18 +1,20 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
         Queue<Integer> q = new PriorityQueue<>((a,b)-> b - a);
+         int ans = 0;
         for(int num : piles){
             q.add(num);
+            ans += num;
         }
         for(int i = 0; i < k; i++){
-           int temp = q.peek() -(q.poll()/2);
+           int temp = q.peek() -(q.peek()/2);
             q.add(temp);
+            ans -= q.poll()/2;
+            
         }
-        int ans = 0;
+       
       
-        while(!q.isEmpty()){
-            ans += q.poll();
-        }
+      
         return ans;
         
     }
