012908538aef5c99641e6dd015029ac118d1017f
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-02-28 11:57:16 +0300

    Time: 49 ms (15.02%), Space: 56.2 MB (18.05%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {     
+        int low = 1;
+        int high = 0;
+          for(int num : nums){
+           high = Math.max(high, num);
+              }
+        System.out.println(sumCalc(3, nums));
+        int ans = high;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+           double sum = sumCalc(mid, nums);
+            if(sum > threshold){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+                ans = Math.min(ans, mid);
+               // System.out.println(ans);
+            }
+        }
+        return ans;
+     
+    }
+    public double sumCalc(int mid, int[] nums){
+       double sum = 0;
+        for(int num: nums){
+            sum += Math.ceil((double)num/(double)mid);
+        }
+        return sum;
+    }
+}
+// 11:00
+// 11 : 18
