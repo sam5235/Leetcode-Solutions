0b584cec4ba0eae6a5980d72c856448c861af758
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-09 15:02:55 +0300

    Time: 3 ms (96.09%), Space: 42 MB (88.03%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.java
+++ 273-integer-to-english-words/273-integer-to-english-words.java
@@ -1,47 +1,48 @@
-class Solution {
+	  class Solution {
+    String[] units = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] tenPlus = {"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     public String numberToWords(int num) {
-        if(num ==   0) return "Zero";
-        String[] one = {" ","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        String[] two = {" ", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        String[] spec ={"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen","Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        List<String> ans = new ArrayList<>();
-       
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(3, "Hundred");
-        map.put(4, "Thousand");
-        map.put(7, "Million");
-        map.put(10, "Billion");
-    
-        String str = Integer.toString(num);
-        int i = str.length() - 1;
-        
-        while(i >= 0){
-            int j = str.length() - i;
-            if(j == 2 || j == 5 || j == 8){
-                
-                ans.add(two[str.charAt(i) - '0']);
-            }else{
-                
-                if(j % 3 == 0 && str.charAt(i) != '0') ans.add(map.get(3));
-                 else if(j  == 4 && (str.charAt(i) != '0' || str.length() <= 6 ||(i > 1) && (str.charAt(i - 1) != '0' || str.charAt(i - 2) != '0') ) ) ans.add(map.get(4));
-                 else if(j == 7  && (str.charAt(i) != '0' || str.length() <= 9 ||(i > 1) && (str.charAt(i - 1) != '0' || str.charAt(i - 2) != '0') ) )  ans.add(map.get(7));
-                 else if(j  == 10) ans.add(map.get(10));
-                if(i > 0 && str.charAt(i - 1) == '1' && !map.containsKey(str.length() - i + 1) ){
-                    ans.add(spec[str.charAt(i) - '0']);
-                    i--;
-                } else  ans.add(one[str.charAt(i) - '0']);
-                
-                
+        int billions = num/1000000000; num%=1000000000; 
+        int millions = num/1000000; num%=1000000;
+        int thousand = num/1000; num%=1000;
+        int hundred = num/100; num%=100;
+        int ten = num/10;
+        int unit = num%10;
+        StringBuffer sb = new StringBuffer();
+        if(billions != 0) {
+            sb.append(numberToWords(billions));
+            sb.append(" Billion");
+        }
+        if(millions != 0) {
+            if(sb.length() != 0) sb.append(' ');
+            sb.append(numberToWords(millions));
+            sb.append(" Million");
+        }
+        if(thousand != 0) {
+            if(sb.length() != 0) sb.append(' ');
+            sb.append(numberToWords(thousand));
+            sb.append(" Thousand");
+        }
+        if(hundred != 0) {
+            if(sb.length() != 0) sb.append(' ');
+            sb.append(units[hundred - 1]);
+            sb.append(" Hundred");
+        }
+        if(ten == 1 && unit != 0) {
+            if(sb.length() != 0) sb.append(' ');
+            sb.append(tenPlus[unit - 1]);
+        } else {
+            if(ten != 0) {
+                if(sb.length() != 0) sb.append(' ');
+                sb.append(tens[ten - 1]);
             }
-            i--;
-            
+            if(unit!=0) {
+                if(sb.length() != 0) sb.append(' ');
+                sb.append(units[unit -1]);
+            }
+            else if(sb.length() == 0) sb.append("Zero");
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append( ans.get(ans.size() - 1));
-        for(int j = ans.size() - 2; j >= 0; j--){
-            if(!ans.get(j).equals(" ")) sb.append( " " + ans.get(j));
-        }
-        
         return sb.toString();
     }
 }
