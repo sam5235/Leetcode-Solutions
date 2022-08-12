a543178958319d99d67417b8c06a8c63c9e13cf8
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-12 17:37:04 +0300

    Time: 128 ms (5.02%), Space: 118.1 MB (5.05%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int[] peri = new int[1];
+        HashSet<List<Integer>> visited = new HashSet<>();
+        int i = 0;
+        while( i < grid.length){
+        int j = 0;
+            while( j < grid[0].length){
+                if(grid[i][j] == 1) {
+                    
+                    visited.add(new ArrayList<>(Arrays.asList(i,j)));
+                    dfs(grid, j, i, peri, visited);
+                    return peri[0];
+                }
+                    j++;
+            }        
+            i++;
+        }
+        return 0;
+    }
+    void dfs(int[][] grid, int col, int row, int[] peri, HashSet<List<Integer>> visited){
+     
+        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(int i = 0; i < 4; i++){
+            int newRow = row + dir[i][0];
+            int newCol = col + dir[i][1];
+            
+            if(outOfBound(newCol, newRow,  grid[0].length, grid.length) || grid[newRow][newCol] == 0 ){
+                peri[0]++;
+            }else{
+                if(!visited.contains(new ArrayList<>(Arrays.asList(newRow, newCol)))){
+                    visited.add(new ArrayList<>(Arrays.asList(newRow, newCol)));
+                     dfs(grid, newCol, newRow, peri, visited);   
+                }
+                
+            }
+        }
+        
+    }
+    
+    boolean outOfBound(int c, int r, int n, int m){
+        return (c >= n || c < 0 || r >= m || r < 0 );
+    }
+}
