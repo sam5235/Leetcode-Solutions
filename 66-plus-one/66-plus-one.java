6a9754aa0a71e43cbbea977ed714d39cee0aa362
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-13 07:01:38 +0300

    Time: 0 ms (100.00%), Space: 42 MB (81.20%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n= digits.length;
+        if(digits[n - 1] < 9){
+            digits[n - 1]++;
+            return digits;
+        }
+        int[] ans = new int[n+1];
+        for(int i = 1; i < ans.length; i++){
+            ans[i] = digits[i - 1];
+        }
+        int i = ans.length - 1;
+        while(i >= 0){
+            if(i > 0 && digits[i -  1] == 9){
+                ans[i] = 0; 
+            }else {
+                ans[i]++;
+                break;
+            }
+            i--;
+        }
+        if(ans[0] == 0){
+            for(int j = 0; j < n; j++) digits[j] = ans[j + 1];
+           return digits;
+        }
+        return ans;
+    }
+}
