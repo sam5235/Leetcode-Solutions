5e381d62bf84d0f4a4553f499933a3cdd14a7242
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-14 21:56:17 +0300

    Time: 13 ms (62.25%), Space: 54 MB (69.59%) - LeetHub

--- 506-relative-ranks/506-relative-ranks.java
+++ 506-relative-ranks/506-relative-ranks.java
@@ -1,6 +1,7 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1] - a[1]);
+        
+  PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1] - a[1]);
         for(int i = 0; i < score.length; i++){
             pq.add(new int[]{i, score[i]});
         }
@@ -10,12 +11,12 @@ class Solution {
         if(!pq.isEmpty()) str[pq.poll()[0]] = "Gold Medal";
            if(!pq.isEmpty()) str[pq.poll()[0]] = "Silver Medal";
           if(!pq.isEmpty())  str[pq.poll()[0]] = "Bronze Medal";
-        
+    
         int i = 4;
         while(i <= score.length && !pq.isEmpty()){
             str[pq.poll()[0]] = i + "";
             i++;
         }
-        return str;
+       return str;
     }
 }
