03a033de36b78a8e72aae04154ba507cbe6ed914
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-19 20:57:20 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (31.68%) - LeetHub

---
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 != null && root2 != null && root1.val != root2.val) return false;
+        if(root1 != null && root1.left != null && root1.right != null){
+            if(  root2 != null && root2.left == null || root2.right == null) return false;
+            if( root2 != null && root1.left.val == root2.left.val && root1.right.val == root2.right.val){
+               return  flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+            }
+            if( root2 != null && root1.left.val == root2.right.val && root1.right.val == root2.left.val){
+                TreeNode temp = root1.right;
+                root1.right= root1.left;
+                root1.left = temp;
+                return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+            }
+            
+        }
+        else if(root1 != null && root1.left != null){
+            if( root2 != null && root2.right == null && root2.left != null && root1.left.val == root2.left.val){
+                 return flipEquiv(root1.left, root2.left);
+            }
+            if( root2 != null && root2.left == null && root2.right != null && root1.left.val == root2.right.val){
+                root1.right = root1.left;
+                root1.left = null;
+                return flipEquiv(root1.right, root2.right);
+            }
+          
+        }
+       else if(root1 != null && root1.right != null){
+            if( root2 != null && root2.left == null && root2.right != null && root1.right.val == root2.right.val){
+                    return flipEquiv(root1.right, root2.right);
+            }
+            if( root2 != null && root2.right == null && root2.left != null && root1.right.val == root2.left.val){
+                root1.left = root1.right;
+                root1.right = null;
+                 return flipEquiv(root1.left, root2.left);
+            }
+          
+        }
+        if(root1 != null && root2 != null && root1.left == null && root1.right == null &&   root2.left == null && root2.right == null) return true;
+          return false;
+        
+    }
+}
