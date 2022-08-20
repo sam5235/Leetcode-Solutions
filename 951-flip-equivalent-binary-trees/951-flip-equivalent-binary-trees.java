a19b41632de1a92ca4735d49c14987708b35641e
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-08-20 12:26:41 +0300

    Time: 0 ms (100.00%), Space: 39.5 MB (100.00%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.java
@@ -15,45 +15,24 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) return true;
-        if(root1 != null && root2 != null && root1.val != root2.val) return false;
-        if(root1 != null && root1.left != null && root1.right != null){
-            if(  root2 != null && root2.left == null || root2.right == null) return false;
-            if( root2 != null && root1.left.val == root2.left.val && root1.right.val == root2.right.val){
-               return  flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
-            }
-            if( root2 != null && root1.left.val == root2.right.val && root1.right.val == root2.left.val){
-                TreeNode temp = root1.right;
-                root1.right= root1.left;
-                root1.left = temp;
-                return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
-            }
-            
-        }
-        else if(root1 != null && root1.left != null){
-            if( root2 != null && root2.right == null && root2.left != null && root1.left.val == root2.left.val){
-                 return flipEquiv(root1.left, root2.left);
-            }
-            if( root2 != null && root2.left == null && root2.right != null && root1.left.val == root2.right.val){
-                root1.right = root1.left;
-                root1.left = null;
-                return flipEquiv(root1.right, root2.right);
-            }
-          
-        }
-       else if(root1 != null && root1.right != null){
-            if( root2 != null && root2.left == null && root2.right != null && root1.right.val == root2.right.val){
-                    return flipEquiv(root1.right, root2.right);
-            }
-            if( root2 != null && root2.right == null && root2.left != null && root1.right.val == root2.left.val){
-                root1.left = root1.right;
-                root1.right = null;
-                 return flipEquiv(root1.left, root2.left);
-            }
-          
-        }
-        if(root1 != null && root2 != null && root1.left == null && root1.right == null &&   root2.left == null && root2.right == null) return true;
-          return false;
+       return whereToGo(root1, root2);
+    }
+     boolean whereToGo(TreeNode node1, TreeNode node2){
+        if(node1 == null || node2 == null) return node1 == null && node2 == null;
+        if(!sim(node1, node2)) return sim(node1,node2);
+        //handling similarity [left == left && right == right]
         
+        if(sim(node1.left, node2.left) && sim(node1.right, node2.right)){
+           return  whereToGo(node1.left, node2.left) && whereToGo(node1.right, node2.right);
+        }
+        if(sim(node1.left, node2.right) && sim(node1.right, node2.left)){
+             return  whereToGo(node1.left, node2.right) && whereToGo(node1.right, node2.left);
+        }
+         return false;
+    }
+    
+    boolean sim(TreeNode node1, TreeNode node2){
+         if(node1 == null || node2 == null) return node1 == null && node2 == null;
+        return node1.val == node2.val;
     }
 }
