99cd97cbfd2a9298f3d8a41580d340f0bc0cf056
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-09-13 13:21:02 +0300

    Time: 60 ms (30.20%), Space: 71.2 MB (64.88%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.java
+++ 886-possible-bipartition/886-possible-bipartition.java
@@ -1,40 +1,44 @@
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
+        if(dislikes.length == 0) return true;
         HashMap<Integer, List<Integer>> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
         Queue<Integer> curr = new LinkedList<>();
         int[] roots = new int[n + 1];
-        for(int i = 0; i < dislikes.length; i++){
-            List<Integer> ls = map.getOrDefault(dislikes[i][0], new ArrayList<>());
-            ls.add(dislikes[i][1]);
-            map.put(dislikes[i][0], ls);
-            List<Integer> lis = map.getOrDefault(dislikes[i][1], new ArrayList<>());
-            lis.add(dislikes[i][0]);
-            map.put(dislikes[i][1], lis);
-        }
+        for(int i = 0; i < roots.length; i++) roots[i] = i;
         for(int i = 1; i <= n; i++){
-            if(!set.contains(i)){
-                roots[i] = 1;
-                curr.add(i);
-                set.add(i);
-                while(!curr.isEmpty()){
-                    int polled = curr.poll();
-                    for(int j = 0; map.containsKey(polled) && j < map.get(polled).size(); j++){
-                        int next = map.get(polled).get(j);
-                        if(roots[next] == roots[polled]) return false;
-                        if(!set.contains(next)){
-                        if(roots[next] == 0){
-                            roots[next] = roots[polled] == 1? 2 : 1;
-                        }
-                        curr.add(next);
-                        set.add(next);    
-                          }
-                    }
-                }
-            }
+            map.put(i, new ArrayList<>());
         }
-       
+        for(int i = 0; i < dislikes.length; i++){
+            int a = dislikes[i][0];
+            int b = dislikes[i][1];
+            map.get(a).add(b);
+            map.get(b).add(a);
+        }
         
+        for(int key : map.keySet()){
+            List<Integer> ls = map.get(key);
+            for(int i = 0; i < ls.size(); i++){
+                if(find(roots,key) == find(roots,ls.get(i))) return false;
+            }
+            for(int i = 0; i < ls.size(); i++){
+            int r = find(roots, ls.get(0));
+                union(roots, r, ls.get(i)); 
+            }
+            
+        }
         return true;
+    }
+    void union(int[] roots, int a, int b){
+        int rootA = find(roots, a);
+        int rootB = find(roots, b);
+        roots[rootB] = rootA;
+    }
+    
+    int find(int[] roots, int node){
+        if(roots[node] == node) return node;
+        int myRoot = find(roots, roots[node]);
+        roots[node] = myRoot;
+        return myRoot;
     }
 }
