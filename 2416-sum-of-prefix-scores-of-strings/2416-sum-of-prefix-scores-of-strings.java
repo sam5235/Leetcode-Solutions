f5e05c4174dade379219035638f2593aacb3fbfb
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-09-28 18:37:12 +0300

    Time: 221 ms (70.83%), Space: 223.7 MB (64.21%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
@@ -1,36 +1,37 @@
 class Solution {
     public int[] sumPrefixScores(String[] words) {
+        Node root = new Node();
+        for(int i = 0; i < words.length; i++){
+            buildTrie(root, words[i]);
+        }
         int[] ans = new int[words.length];
-        Trie root = new Trie();
-        buildTrie(root, words);
-          for(int i = 0; i < words.length; i++){
-              Trie node = root;
-              for(int j = 0; j < words[i].length(); j++){
-                int idx  = words[i].charAt(j) - 'a';
-                  node = node.children[idx];                  
-                  ans[i] += node.count;
-              }
-          }
+        for(int i = 0; i < words.length; i++){
+            ans[i] = countPref(root, words[i], 0);
+        }
         return ans;
-        
     }
-        void buildTrie(Trie root, String[] words){
-         for(int i = 0; i < words.length; i++){
-            Trie node = root;
-            for(int j = 0; j < words[i].length(); j++){
-                int idx  = words[i].charAt(j) - 'a';
-                if(node.children[idx] == null){
-                    node.children[idx] = new Trie();
-                }
-                    node = node.children[idx];
-                    node.count++;
+    
+    void buildTrie(Node node, String s){
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(node.children[c - 'a'] == null){
+                node.children[c - 'a'] = new Node();
             }
-         }
+            node.children[c - 'a'].freq++;
+            node = node.children[c - 'a'];
+        }
+    }
+    
+    int countPref(Node node, String s, int sum){
+       for(int i = 0; i < s.length(); i++){
+            sum += node.children[s.charAt(i) - 'a'].freq;
+            node = node.children[s.charAt(i) - 'a'];
+       }
+        return sum;
     }
 }
 
-
-class Trie{
-  Trie[] children = new Trie[26];
-  int count = 0;  
+class Node{
+ Node[] children = new Node[26];
+ int freq = 0;   
 }
