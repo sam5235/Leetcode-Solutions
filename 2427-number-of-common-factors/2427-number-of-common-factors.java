910c1f5314d4a1403abb28c6ae14cd51e9620d90
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-10-05 13:42:47 +0300

    Time: 0 ms (100.00%), Space: 40.3 MB (81.17%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -3,8 +3,12 @@ class Solution {
         int small = a <= b ? a : b;
         int large = a >= b ? a : b;
         int ans = 0;
-        for(int i = 1; i <= small; i++){
-            if(small % i == 0 && large % i == 0) ans++;
+        for(int i = 1; i <= Math.sqrt(small); i++){
+            if(small % i == 0){
+              int j = small / i;
+                if(large % i == 0)ans++;
+                if(large % j == 0 && i != j)ans++;
+            }
         }
         return ans;
     }
