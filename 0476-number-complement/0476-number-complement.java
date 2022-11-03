aba10c0d6277672d0625d23e4ff407fe20e19330
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-11-03 15:43:09 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (31.54%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,14 +1,6 @@
 class Solution {
     public int findComplement(int num) {
-        int ans = 0;
-        int shift = 0;
-        while(num > 0){
-            if((num & 1 )== 0){
-                ans ^= (1 << shift);
-            }
-            shift++;
-            num >>= 1;
-        }
-        return ans;
+         return (int)(Math.pow(2,(int)(Math.log(num)/ Math.log(2)) + 1) - 1) ^ num;
+      
     }
 }
