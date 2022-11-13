c214b7b9bb7080c86df7a042b46e4918cb37a88d
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-11-13 12:44:48 +0300

    Time: 1 ms (28.97%), Space: 42.8 MB (22.33%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,24 +3,19 @@ class Solution {
         int[] ans = new int[digits.length + 1];
         int bit = 1;
         for(int i = digits.length - 1; i >= 0; i--){
-            ans[i + 1] = digits[i]; 
             if(bit > 0){
-                if(ans[i + 1] < 9){
-                    ans [i + 1] += 1;
+                if(digits[i] < 9){
+                    digits[i] += 1;
                     bit--;
                 }else
-                    ans[i + 1] = 0;
+                    digits[i] = 0;
             }
         }
         if(bit == 1){
             ans[0] = 1;
+            return ans;
         }
-        if(ans[0] == 0){
-            int[] newAns = new int[ans.length - 1];
-            for(int i = 0; i < ans.length - 1; i++)
-                newAns[i] = ans[i +1];
-         return newAns;
-        }
-        return ans;
+        
+        return digits;
     }
 }
