37b3cae57fb539d1cc8db2159d67d31c3a36f49f
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-12-14 16:59:52 +0300

    Time: 0 ms (100.00%), Space: 39.5 MB (93.88%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return Math.max(fun(nums, memo, 0), fun(nums,memo, 1));
+    }
+    int fun(int[] nums, HashMap<Integer, Integer> memo, int idx){
+        if(idx >= nums.length) return 0;
+        if(memo.containsKey(idx)) return memo.get(idx);
+        int i = idx + 2;
+        int fIdx = Math.max(fun(nums, memo, i), fun(nums, memo, i + 1)) + nums[idx];
+        memo.put(idx, fIdx);
+        return fIdx;
+    }
+}
+
