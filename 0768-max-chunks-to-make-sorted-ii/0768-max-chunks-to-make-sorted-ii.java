021e52d89b04504de3afe7cde2027c925fb5012d
sam5235 <87311061+sam5235@users.noreply.github.com>
2022-12-26 15:43:43 +0300

    Time: 8 ms (20.86%), Space: 41.8 MB (96.03%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -1,33 +1,19 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int[] sorted = arr.clone();
-        Arrays.sort(sorted);
-        HashMap<Integer, Integer> arrMap = new HashMap<>();
-        HashMap<Integer, Integer> sorMap = new HashMap<>();
-        int count = 0;
+        Stack<Integer> st = new Stack<>();
+        int max = 0;
         for(int i = 0; i < arr.length; i++){
-             int freq = arrMap.getOrDefault(arr[i], 0) + 1;
-             arrMap.put(arr[i], freq);
-             freq =  sorMap.getOrDefault(sorted[i], 0) + 1;
-             sorMap.put(sorted[i], freq);
-             if(isSor(arrMap, sorMap)){
-                  arrMap = new HashMap<>();
-                  sorMap = new HashMap<>();
-                  count++;
-             }
+            if(st.isEmpty() || st.peek() <= arr[i])
+                st.push(arr[i]);
+            else {
+                while(!st.isEmpty() && st.peek() > arr[i]){
+                    max = Math.max(max, st.pop());
+                
+                }
+                st.push(max);
+                
+            }
         }
-        
-        return count;
-        
+        return st.size();
     }
-       boolean isSor(HashMap<Integer, Integer> arrMap, HashMap<Integer, Integer> sorMap){
-           if(sorMap.size() != arrMap.size()) return false;
-           for(int key : arrMap.keySet()){
-               if(!sorMap.containsKey(key) || !sorMap.get(key).equals(arrMap.get(key))){
-                   return false;
-               }
-           }
-           return true;
-       }
-    
 }
