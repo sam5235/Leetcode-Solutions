f0ac611396b9932e753ba752c7208838850abbcd
sam5235 <87311061+sam5235@users.noreply.github.com>
2023-02-01 20:28:21 +0300

    Time: 150 ms (19.88%), Space: 43.2 MB (56.18%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,21 +1,32 @@
 class Solution {
-    public int[][] matrixBlockSum(int[][] mat, int K) {
-        int m = mat.length, n = mat[0].length;
-        int[][] sum = new int[m + 1][n + 1]; 
-        for (int r = 1; r <= m; r++) {
-            for (int c = 1; c <= n; c++) {
-                sum[r][c] = mat[r - 1][c - 1] + sum[r - 1][c] + sum[r][c - 1] - sum[r - 1][c - 1];
-            }
-        }
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+
+        int m = mat.length;
+        int n = mat[0].length;
+
         int[][] ans = new int[m][n];
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                int r1 = Math.max(0, r - K), c1 = Math.max(0, c - K);
-                int r2 = Math.min(m - 1, r + K), c2 = Math.min(n - 1, c + K);
-                r1++; c1++; r2++; c2++; 
-                ans[r][c] = sum[r2][c2] - sum[r2][c1-1] - sum[r1-1][c2] + sum[r1-1][c1-1];
+
+        for (int i = 0; i < m; i++) {
+            
+            for (int j = 0; j < n; j++) {
+
+                int sum = 0;
+
+                for (int l = Math.max(0,i-k); l <= Math.min(m-1,i+k); l++) {
+
+                    for (int o = Math.max(0,j-k); o <= Math.min(n-1,j+k); o++) {
+                        sum+=mat[l][o];
+                        
+                    }
+                    
+                }
+
+                ans[i][j] = sum;
             }
+            
         }
+        
         return ans;
+
     }
 }
