a123fd5ccb5d047b3969ae8b68cc41babb0faa05
sam5235 <87311061+sam5235@users.noreply.github.com>
2023-10-29 07:10:14 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (39.95%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,74 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) { 
+        Node node = head;
+        helper(node, new Stack<>());
+        return head;
+    }
+    
+    void helper(Node node, Stack<Node> st){
+        if(node == null)
+            return;
+        if(node.child == null && node.next == null && node.prev != null){
+
+            if(st.isEmpty())
+                return;
+            Node back = st.pop();
+            node.next = back;
+            back.prev = node;
+            helper(node.next,st);
+            
+        }
+        
+            if(node.child != null){
+                if(node.next != null)
+                    st.push(node.next);
+                node.next = node.child;
+                node.child.prev = node;
+                node.child = null;
+            }
+           helper(node.next,st);
+        
+    }
+}
+/*
+
+back = 4
+3->7->(back = 9)
+8->11->12->9-10-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
